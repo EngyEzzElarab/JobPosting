ac8bc2afa60cb77c0464d399882f48f6c7f8b2a6
winfieldk <87447123+winfieldk@users.noreply.github.com>
2021-07-20 08:27:54 -0400

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JobPosting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
